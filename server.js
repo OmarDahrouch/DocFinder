3c03c7caa5a2ac354898f27587635e8fb16b6594
Mehdi Machane <mehdimachane2@gmail.com>
2023-05-19 01:38:30 +0100

    prettier

--- server.js
+++ server.js
@@ -1,50 +1,47 @@
-const express = require ('express')
-const bodyParser = require ('body-parser')
-const app =express()
-const PORT = 3000
-const {mogoUrl} = require ('./database')
-const mongoose = require ('mongoose')
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+const { mogoUrl } = require("./database");
+const mongoose = require("mongoose");
 
 // Middleware pour l'api patients------------------------------------------
 
-require('./models/patients');
+require("./models/patients");
 
-const authRoutes = require('./routes/authRoute')
-app.use(bodyParser.json())
-app.use(authRoutes)
+const authRoutes = require("./routes/authRoute");
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 // Middleware pour l'api des appointments---------------------------------
 
-require('./models/appointments');
+require("./models/appointments");
 
-const appointmentRoute = require('./routes/appointmentRoute')
-app.use(bodyParser.json())
-app.use(appointmentRoute)
-
-
+const appointmentRoute = require("./routes/appointmentRoute");
+app.use(bodyParser.json());
+app.use(appointmentRoute);
 
 // DataBase Config ----------------------------------------------------
 
-mongoose.connect(mogoUrl)
+mongoose.connect(mogoUrl);
 
-mongoose.connection.on('connected', ()=>{
-    console.log("Connection to DataBase Successful !!!")
-})
+mongoose.connection.on("connected", () => {
+  console.log("Connection to DataBase Successful !!!");
+});
 
-mongoose.connection.on('error', (err)=>{
-    console.log("You got an error", err)
-})
+mongoose.connection.on("error", (err) => {
+  console.log("You got an error", err);
+});
 
 // Server Config ------------------------------------------------------
 
-app.get('/',(req,res)=> {
-    console.log('Body-parser success !!!')
-    res.send('Hello everybody')
-})
+app.get("/", (req, res) => {
+  console.log("Body-parser success !!!");
+  res.send("Hello everybody");
+});
 
-app.listen(PORT,() =>{
-    console.log("Server running "+PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server running " + PORT);
+});
 
 //----------------------------------------------------------------------
